aa7102d8891f967e188853cb814cff5d4acac64b
hanjiahui <apple@appledeMacBook-Pro.local>
2018-10-31 14:58:23 +0800

    添加喜闻乐荐活动流程

---
+++ src/views/elife/process.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="elife-process-page">
+    <img class="process-img" src="../../assets/img/elife/elife-process.jpg" alt="">
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
