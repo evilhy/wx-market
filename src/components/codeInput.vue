1cbb398b1ae6628f5d2848106f64db25d62a5193
hanjiahui <apple@appledeMacBook-Pro.local>
2019-01-28 11:53:35 +0800

    优化样式

--- src/components/codeInput.vue
+++ src/components/codeInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="code-input-wrap">
     <input ref="code-input" class="input code-input" :class="{ 'hidden': !flag }" type="tel" :maxlength="len" v-model.trim="code" @focus="flag=false" @blur="flag=true" />
-    <div class="code-wrap">
+    <div class="code-wrap" :class="{'focus': !flag}">
       <span v-for="(codeChar, index) in codeList" :key="index" class="code-item">
         <template v-if="codeChar">
           <span v-show="visible">{{codeChar}}</span>
