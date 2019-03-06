8bedc7b5242a043c8a5b67da97da284f758377b4
hanjiahui <apple@appledeMacBook-Pro.local>
2019-03-06 17:17:43 +0800

    新增关于我们页面

---
+++ src/views/public/aboutUs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about-us-page"></div>
+</template>
+<script>
+import sysConfig from 'utils/constant'
+export default {
+  data () {
+
+  },
+  created () {
+    window.location.replace(sysConfig.about_us_url)
+  }
+}
+</script>
