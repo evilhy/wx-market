e48caa9ab92989033c88f22220409dd61fa4f538
bzw <bzw@beijingkaikeweishideiMac.local>
2019-04-03 10:15:11 +0800

    修改

--- src/views/public/adPicture.vue
+++ src/views/public/adPicture.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="bank-site-page"></div>
+  <div class="bank-site-page"><img :src="sysConfig.ad_picture_url"></div>
 </template>
 <script>
-import sysConfig from 'utils/constant'
-export default {
-  data () {
-
-  },
-  created () {
-    window.location.replace(sysConfig.ad_picture_url)
+  import sysConfig from 'utils/constant'
+  export default {
+    data () {
+      return {
+        sysConfig
+      }
+    },
+    created () {
+      /* window.location.replace(sysConfig.ad_picture_url) */
+    }
   }
-}
 </script>
