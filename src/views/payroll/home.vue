9730f2fe64158f5d76d6349a3c65daca56d84b05
hanjiahui <apple@appledeMacBook-Pro.local>
2019-04-11 18:09:17 +0800

    更换轮播图

--- src/views/payroll/home.vue
+++ src/views/payroll/home.vue
@@ -89,6 +89,8 @@ export default {
         this.imgList = res.data
       } else {
         this.imgList = [{
+          url: require('../../assets/img/home-banner8.png')
+        }, {
           url: require('../../assets/img/home-banner6.png')
         }, {
           url: require('../../assets/img/home-banner3.png')
