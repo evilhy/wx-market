7a3418238b17e1eae7198fdbca3ae0d4c33f1bec
hanjiahui <apple@appledeMacBook-Pro.local>
2019-06-19 10:56:17 +0800

    更新微信分享方式

--- src/mixins/tShare.js
+++ src/mixins/tShare.js
@@ -1,6 +1,7 @@
 import Wxapi from 'utils/wxapi'
 import helper from 'utils/helper'
-const wxapiInstance = new Wxapi()
+const wxapi = new Wxapi()
+
 export default {
   data() {
     return {
@@ -15,18 +16,18 @@ export default {
       let { appid, timestamp, noncestr, signature } = res.data
       let { productName = '', productDesc = '', productImg = '' } = helper.getTFinanceInfo(null, {})
       let link = this.getShareLink()
-      await wxapiInstance.config({ appId: appid, timestamp, nonceStr: noncestr, signature })
-      wxapiInstance.shareTimeline({
-        title: productName,
-        link,
-        imgUrl: productImg
-      })
-      wxapiInstance.shareAppMessage({
-        title: productName,
-        desc: productDesc,
-        link,
-        imgUrl: productImg
-      })
+      try {
+        await wxapi.checkJsApi()
+        try {
+          await wxapi.config({ appId: appid, timestamp, nonceStr: noncestr, signature })
+          wxapi.updateAppMessageShareData({ title: productName, desc: productDesc, link, imgUrl: productImg })
+          wxapi.updateTimelineShareData({ title: productName, link, imgUrl: productImg })
+        } catch (e) { 
+          helper.toast('微信分享配置失败')
+        }
+      } catch (e) { 
+        helper.toast('请升级微信客户端后分享')
+      }
     },
     getShareLink () {
       let { query, path } = this.$route
