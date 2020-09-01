e577443c2317aa99b4a692e8c1ec6b230feea6ca
hanjiahui <apple@appledeMacBook-Pro.local>
2020-09-01 16:44:42 +0800

    vant按需引用

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vant, { Lazyload, Image } from 'vant'
+import { Lazyload, DropdownMenu, DropdownItem, NoticeBar, Swipe, SwipeItem, Image, Popup, Icon, AddressList, AddressEdit, ActionSheet, Cell, CellGroup, Field, Switch, Button, PasswordInput, List, Tab, Tabs, PullRefresh, SwipeCell, NumberKeyboard, NavBar, RadioGroup, Radio, Sticky, Calendar, Step, Steps } from 'vant'
 import myPlugin from './utils/plugin'
-import 'vant/lib/index.css'
 
 import './assets/fonts/iconfont.css'
 import './assets/scss/common.scss'
@@ -15,13 +14,42 @@ import 'swiper/dist/css/swiper.css'
 // import VeLine from 'v-charts/lib/line'
 import VueClipboard from 'vue-clipboard2'
 Vue.config.productionTip = false
-
-Vue.use(Vant)
+// vant
 Vue.use(Lazyload, {
   preLoad: 1.3,
   attempt: 1
 })
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(NoticeBar)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
 Vue.use(Image)
+Vue.use(Popup)
+Vue.use(Icon)
+Vue.use(AddressList)
+Vue.use(AddressEdit)
+Vue.use(ActionSheet)
+Vue.use(Cell)
+Vue.use(CellGroup)
+Vue.use(Field)
+Vue.use(Switch)
+Vue.use(Button)
+Vue.use(PasswordInput)
+Vue.use(List)
+Vue.use(Tab)
+Vue.use(Tabs)
+Vue.use(PullRefresh)
+Vue.use(SwipeCell)
+Vue.use(NumberKeyboard)
+Vue.use(NavBar)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Sticky)
+Vue.use(Calendar)
+Vue.use(Step)
+Vue.use(Steps)
+
 Vue.use(myPlugin)
 Vue.use(VueClipboard)
 
