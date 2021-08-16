f848fe2a2db9bd9f7623c4473e886a8196f2d7be
bzw <biziwei@kayak.com.cn>
2021-08-16 10:20:29 +0800

    更新

--- src/views/user/bankcardEdit.vue
+++ src/views/user/bankcardEdit.vue
@@ -2,12 +2,12 @@
   <div class="bankcard-edit-page">
     <div class="tip" v-if="step === '1'">请输入您本人的银行卡</div>
     <van-cell-group>
-      <van-field label="持卡人姓名"  v-model="userName" readonly />
+      <van-field label="持卡人姓名"  v-model="currentBank.userName" readonly />
       <van-field v-if="step==='2'" label="开卡行" v-model="issuerName" readonly />
-      <van-field label="银行卡号" v-model="cardNo" type="digit" placeHolder="请输入持卡人本人的银行卡" maxlength="21" clear-trigger="always" clearable :readonly="step==='2'"/>
+      <van-field label="银行卡号" v-model="currentBank.cardNo" type="digit" placeHolder="请输入持卡人本人的银行卡" maxlength="21" clear-trigger="always" clearable :readonly="step==='2'"/>
     </van-cell-group>
     <div class="btn-box">
-      <van-button :color="cardNo ? '#00CCCC' : '#A8AAB2'" v-if="step === '1'" :loading="loading.step1" loading-text="校验中..." @click="checkCardBin">下一步</van-button>
+      <van-button :color="currentBank.cardNo ? '#00CCCC' : '#A8AAB2'" v-if="step === '1'" :loading="loading.step1" loading-text="校验中..." @click="checkCardBin">下一步</van-button>
       <van-button color="#00CCCC" plain v-if="step === '2'" :loading="loading.step2" loading-text="提交中..." @click="updBankCard">申请修改</van-button>
     </div>
   </div>
@@ -21,21 +21,18 @@ export default {
     return {
       loading: {step1: false, step2: false},
       step: '1',
-      userName: '',
       issuerName: '',
-      cardNo: ''
+      currentBank: storage.getSession('currentBank')
     }
   },
   created () {
-    let {cardNo, userName} = storage.getSession('currentBank')
-    this.cardNo = cardNo
-    this.userName = userName
   },
   methods: {
     async checkCardBin () {
+      let {cardNo} = this.currentBank
       try {
         this.loading.step1 = true
-        let res = await this.$Roll.checkCardBin(this.cardNo)
+        let res = await this.$Roll.checkCardBin(cardNo)
         this.step = '2'
         this.issuerName = res.data.issuerName
       } finally {
@@ -45,7 +42,7 @@ export default {
     async updBankCard () {
       try {
         this.loading.step2 = true
-        await this.$Inside.updBankCard(this.cardNo)
+        await this.$Inside.updBankCard(this.currentBank)
         helper.toast('提交成功')
         this.$router.push({name: 'bankcardList'})
       } finally {
