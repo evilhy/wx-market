9bcc0649db4329d3fa0e49f75c7eabcaf46c265b
hanjiahui <821877109@qq.com>
2021-09-03 11:48:44 +0800

    优化

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  'plugins': {
     // to edit target browsers: use "browserslist" field in package.json
-    "postcss-import": {},
-    "autoprefixer": {
+    'postcss-import': {},
+    'autoprefixer': {
       browsers: ['Android >= 5.0', 'iOS >= 8']
     },
     'postcss-px-to-viewport': {
       viewportWidth: 640,
       selectorBlackList: [],
-      mediaQuery: true
+      mediaQuery: true,
+      landscapeWidth: 1136
     }
   }
 }
